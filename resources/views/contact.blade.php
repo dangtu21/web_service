30a58f07157260b899dd5f0887bcea0b8e36e2b0
anhtuhanam1@gmail.com <anhtuhanam1@gmail.com>
2024-09-10 22:37:01 +0700

    aaaa

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,55 +5,74 @@
 @endsection
 
 @section('content')
-<div class="container">
-<div class="row align-items-stretch no-gutters contact-wrap">
-<div class="col-md-12">
-<div class="form h-100">
-<h3>Get Started</h3>
-<form class="mb-5" method="post" id="contactForm" name="contactForm" novalidate="novalidate">
-<div class="row">
-<div class="col-md-6 form-group mb-3">
-<label for="" class="col-form-label">Name *</label>
-<input type="text" class="form-control" name="name" id="name" placeholder="Your name">
+<div class="page-content">
+    <!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+        <div class="breadcrumb-title pe-3">Tables</div>
+        <div class="ps-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 p-0">
+                    <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                    </li>
+                    <li class="breadcrumb-item active">Liên hệ với chúng tôi</li>
+                </ol>
+            </nav>
+        </div>
+
+    </div>
+    <div class="container">
+        <div class="row align-items-stretch no-gutters contact-wrap">
+            <div class="col-md-12">
+                <div class="form h-100">
+                    <h3>Get Started</h3>
+                    <form class="mb-5" method="post" id="contactForm" name="contactForm" novalidate="novalidate">
+                        <div class="row">
+                            <div class="col-md-6 form-group mb-3">
+                                <label for="" class="col-form-label">Name *</label>
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Your name">
+                            </div>
+                            <div class="col-md-6 form-group mb-3">
+                                <label for="" class="col-form-label">Email *</label>
+                                <input type="text" class="form-control" name="email" id="email"
+                                    placeholder="Your email">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 form-group mb-3">
+                                <label for="budget" class="col-form-label">Budget</label>
+                                <select class="custom-select" id="budget" name="budget">
+                                    <option selected="">Choose...</option>
+                                    <option value="$1000 below">&lt; $1,000</option>
+                                    <option value="$2,000 - $5,000">$2,000 - $5,000</option>
+                                    <option value="$5,000 - $15,000">$5,000 - $15,000</option>
+                                    <option value="$15,000 - $25,000">$15,000 - $25,000</option>
+                                    <option value="$25,000 >">$25,000 &gt;</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 form-group mb-3">
+                                <label for="message" class="col-form-label">Message *</label>
+                                <textarea class="form-control" name="message" id="message" cols="30" rows="4"
+                                    placeholder="Write your message"></textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 form-group">
+                                <input type="submit" value="Send Message" class="btn btn-primary rounded-0 py-2 px-4">
+                                <span class="submitting"></span>
+                            </div>
+                        </div>
+                    </form>
+                    <div id="form-message-warning mt-4"></div>
+                    <div id="form-message-success">
+                        Your message was sent, thank you!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<div class="col-md-6 form-group mb-3">
-<label for="" class="col-form-label">Email *</label>
-<input type="text" class="form-control" name="email" id="email" placeholder="Your email">
-</div>
-</div>
-<div class="row">
-<div class="col-md-12 form-group mb-3">
-<label for="budget" class="col-form-label">Budget</label>
-<select class="custom-select" id="budget" name="budget">
-<option selected="">Choose...</option>
-<option value="$1000 below">&lt; $1,000</option>
-<option value="$2,000 - $5,000">$2,000 - $5,000</option>
-<option value="$5,000 - $15,000">$5,000 - $15,000</option>
-<option value="$15,000 - $25,000">$15,000 - $25,000</option>
-<option value="$25,000 >">$25,000 &gt;</option>
-</select>
-</div>
-</div>
-<div class="row">
-<div class="col-md-12 form-group mb-3">
-<label for="message" class="col-form-label">Message *</label>
-<textarea class="form-control" name="message" id="message" cols="30" rows="4" placeholder="Write your message"></textarea>
-</div>
-</div>
-<div class="row">
-<div class="col-md-12 form-group">
-<input type="submit" value="Send Message" class="btn btn-primary rounded-0 py-2 px-4">
-<span class="submitting"></span>
-</div>
-</div>
-</form>
-<div id="form-message-warning mt-4"></div>
-<div id="form-message-success">
-Your message was sent, thank you!
-</div>
-</div>
-</div>
-</div>
-</div>
+
 
 @endsection
--- resources/views/purchaseHistory.blade.php
+++ resources/views/purchaseHistory.blade.php
@@ -13,20 +13,7 @@
                 </ol>
             </nav>
         </div>
-        <div class="ms-auto">
-            <div class="btn-group">
-                <button type="button" class="btn btn-light">Settings</button>
-                <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split"
-                    data-bs-toggle="dropdown"> <span class="visually-hidden">Toggle Dropdown</span>
-                </button>
-                <div class="dropdown-menu dropdown-menu-right dropdown-menu-lg-end"> <a class="dropdown-item"
-                        href="javascript:;">Action</a>
-                    <a class="dropdown-item" href="javascript:;">Another action</a>
-                    <a class="dropdown-item" href="javascript:;">Something else here</a>
-                    <div class="dropdown-divider"></div> <a class="dropdown-item" href="javascript:;">Separated link</a>
-                </div>
-            </div>
-        </div>
+        
     </div>
     <!--end breadcrumb-->
     <h6 class="mb-0 text-uppercase">Lịch sử mua hàng </h6>
