f8e7430470d8c917de917ef50cc1fe5389b958e9
anhtuhanam1@gmail.com <anhtuhanam1@gmail.com>
2024-09-10 13:07:19 +0700

    aaaa

--- resources/views/mainMenu.blade.php
+++ resources/views/mainMenu.blade.php
@@ -467,72 +467,11 @@
                                     <a href="javascript:;">
                                         <div class="msg-header">
                                             <p class="msg-header-title">Notifications</p>
-                                            <p class="msg-header-badge">8 New</p>
+                                            <p class="msg-header-badge">0 New</p>
                                         </div>
                                     </a>
                                     <div class="header-notifications-list">
-                                        <a class="dropdown-item" href="javascript:;">
-                                            <div class="d-flex align-items-center">
-                                                <div class="user-online">
-                                                    <img src="{{ asset('assets/images/avatars/avatar-7.png')}}"
-                                                        class="msg-avatar" alt="user avatar">
-                                                </div>
-                                                <div class="flex-grow-1">
-                                                    <h6 class="msg-name">Daisy Anderson<span
-                                                            class="msg-time float-end">5 sec
-                                                            ago</span></h6>
-                                                    <p class="msg-info">The standard chunk of lorem</p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                        <a class="dropdown-item" href="javascript:;">
-                                            <div class="d-flex align-items-center">
-                                                <div class="notify bg-light-danger text-danger">dc
-                                                </div>
-                                                <div class="flex-grow-1">
-                                                    <h6 class="msg-name">New Orders <span class="msg-time float-end">2
-                                                            min
-                                                            ago</span></h6>
-                                                    <p class="msg-info">You have recived new orders</p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                        <a class="dropdown-item" href="javascript:;">
-                                            <div class="d-flex align-items-center">
-                                                <div class="user-online">
-                                                    <img src="{{ asset('assets/images/avatars/avatar-7.png')}}"
-                                                        class="msg-avatar" alt="user avatar">
-                                                </div>
-                                                <div class="flex-grow-1">
-                                                    <h6 class="msg-name">Althea Cabardo <span
-                                                            class="msg-time float-end">14
-                                                            sec ago</span></h6>
-                                                    <p class="msg-info">Many desktop publishing packages</p>
-                                                </div>
-                                            </div>
-                                        </a>
-                                        <a class="dropdown-item" href="javascript:;">
-                                            <div class="d-flex align-items-center">
-                                                <div class="notify bg-light-success text-success">
-                                                    <img src="{{ asset('assets/images/app/outlook.png" width="25" alt="user avatar">
-												</div>
-												<div class="flex-grow-1">
-													<h6 class="msg-name">Account Created<span class="msg-time float-end">28 min
-												ago</span></h6>
-													<p class="msg-info">Successfully created new email</p>
-												</div>
-											</div>
-										</a>
-										<a class="dropdown-item" href="javascript:;">
-											<div class="d-flex align-items-center">
-												<div class="notify bg-light-info text-info">Ss
-												</div>
-												<div class="flex-grow-1">
-													<h6 class="msg-name">New Product Approved <span class="msg-time float-end">2 hrs ago</span></h6>
-													<p class="msg-info">Your new product has approved</p>
-												</div>
-											</div>
-										</a>
+                                        
 										<a class="dropdown-item" href="javascript:;">
 											<div class="d-flex align-items-center">
                       <div class="user-online">
