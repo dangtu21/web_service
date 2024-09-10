08e260d97741ecb33ac2f00767dae6d7e9b3fd6d
anhtuhanam1@gmail.com <anhtuhanam1@gmail.com>
2024-09-10 22:34:10 +0700

    aaaa

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,47 +5,55 @@
 @endsection
 
 @section('content')
-<div class="contact1">
-    <div class="container-contact1">
-        <div class="contact1-pic js-tilt" data-tilt>
-            <img src="images/img-01.png" alt="IMG">
-        </div>
-
-        <form class="contact1-form validate-form">
-            <span class="contact1-form-title">
-                Get in touch
-            </span>
-
-            <div class="wrap-input1 validate-input" data-validate="Name is required">
-                <input class="input1" type="text" name="name" placeholder="Name">
-                <span class="shadow-input1"></span>
-            </div>
-
-            <div class="wrap-input1 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                <input class="input1" type="text" name="email" placeholder="Email">
-                <span class="shadow-input1"></span>
-            </div>
-
-            <div class="wrap-input1 validate-input" data-validate="Subject is required">
-                <input class="input1" type="text" name="subject" placeholder="Subject">
-                <span class="shadow-input1"></span>
-            </div>
-
-            <div class="wrap-input1 validate-input" data-validate="Message is required">
-                <textarea class="input1" name="message" placeholder="Message"></textarea>
-                <span class="shadow-input1"></span>
-            </div>
-
-            <div class="container-contact1-form-btn">
-                <button class="contact1-form-btn">
-                    <span>
-                        Send Email
-                        <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-                    </span>
-                </button>
-            </div>
-        </form>
-    </div>
+<div class="container">
+<div class="row align-items-stretch no-gutters contact-wrap">
+<div class="col-md-12">
+<div class="form h-100">
+<h3>Get Started</h3>
+<form class="mb-5" method="post" id="contactForm" name="contactForm" novalidate="novalidate">
+<div class="row">
+<div class="col-md-6 form-group mb-3">
+<label for="" class="col-form-label">Name *</label>
+<input type="text" class="form-control" name="name" id="name" placeholder="Your name">
+</div>
+<div class="col-md-6 form-group mb-3">
+<label for="" class="col-form-label">Email *</label>
+<input type="text" class="form-control" name="email" id="email" placeholder="Your email">
+</div>
+</div>
+<div class="row">
+<div class="col-md-12 form-group mb-3">
+<label for="budget" class="col-form-label">Budget</label>
+<select class="custom-select" id="budget" name="budget">
+<option selected="">Choose...</option>
+<option value="$1000 below">&lt; $1,000</option>
+<option value="$2,000 - $5,000">$2,000 - $5,000</option>
+<option value="$5,000 - $15,000">$5,000 - $15,000</option>
+<option value="$15,000 - $25,000">$15,000 - $25,000</option>
+<option value="$25,000 >">$25,000 &gt;</option>
+</select>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12 form-group mb-3">
+<label for="message" class="col-form-label">Message *</label>
+<textarea class="form-control" name="message" id="message" cols="30" rows="4" placeholder="Write your message"></textarea>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12 form-group">
+<input type="submit" value="Send Message" class="btn btn-primary rounded-0 py-2 px-4">
+<span class="submitting"></span>
+</div>
+</div>
+</form>
+<div id="form-message-warning mt-4"></div>
+<div id="form-message-success">
+Your message was sent, thank you!
+</div>
+</div>
+</div>
+</div>
 </div>
 
 @endsection
